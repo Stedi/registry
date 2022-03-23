b09e53972e189235a7cf9387e8c349829533720b
Rafal Wiliński <raf.wilinski@gmail.com>
2022-03-23 16:05:12 -0500

    feat: flexport v2

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,9 +1,11 @@
 import { StripeProvider } from "./stripe";
 import { ShopifyProvider } from "./shopify";
 import ramp from "./ramp";
+import { FlexportProvider } from "./flexport";
 
 export default {
   stripe: new StripeProvider(),
   shopify: new ShopifyProvider(),
+  flexport: new FlexportProvider(),
   ramp,
 };
