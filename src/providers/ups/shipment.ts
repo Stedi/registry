f9c866398af36d04586081588e4e13f2223c5df5
Rafal Wiliński <raf.wilinski@gmail.com>
2022-04-08 11:26:07 -0500

    feat: ups shipment provider

--- src/providers/ups/shipment.ts
+++ src/providers/ups/shipment.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { OpenAPI3Schema } from "../../provider";
 import { OpenAPIProvider } from "../openapi";
 
 export class UPSShipmentProvider extends OpenAPIProvider {
@@ -12,8 +13,31 @@ export class UPSShipmentProvider extends OpenAPIProvider {
       baseUrl: "https://www.ups.com/upsdeveloperkit/assets/json/Shipment.js",
       docsLink: "https://www.ups.com/upsdeveloperkit/",
       customPath: "ups/shipment/1.0.1/",
+      entities: [
+        "ShipmentResponse",
+        "ShipTo_Address",
+        "ShipmentRequest",
+        "InternationalForms_Product",
+        "Shipment_PaymentInformation",
+        "PaymentInformation_ShipmentCharge",
+        "ShipmentResults_ShipmentCharges",
+        "Shipment_Package",
+        "Shipment_ShipFrom",
+        "Shipment_Shipper",
+      ],
       sanitizeSchema,
     });
+  }
+
+  override async getSchema(version: string): Promise<OpenAPI3Schema> {
+    const definition = await axios.get(this.baseUrl!);
+
+    return {
+      type: "openapi-v3",
+      versionName: version,
+      value: JSON.parse(definition.data.replace("shipment= ", "")),
+      entities: this.entities,
+    };
   }
 }
 
