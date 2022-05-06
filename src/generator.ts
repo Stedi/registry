43469ceae062c03bf987448d68cac21aece54484
Rafal Wilinski <raf.wilinski@gmail.com>
2022-05-06 11:10:34 +0200

    refactor: get rid of example keyword, change to examples

--- src/generator.ts
+++ src/generator.ts
@@ -80,7 +80,21 @@ function generateEntitySchema(schema: EntitySchema, baseDir: string) {
   (schema.schema as any)["default"] = mock(schema.schema as OpenAPIV3.SchemaObject);
   (schema.schema as any)["$schema"] = "https://json-schema.org/draft/2020-12/schema";
 
-  fs.writeFileSync(target, JSON.stringify(schema.schema, null, 2));
+  fs.writeFileSync(
+    target,
+    JSON.stringify(
+      schema.schema,
+      (key, value) => {
+        if (value?.example !== undefined) {
+          value.examples = [value.example];
+          delete value.example;
+        }
+
+        return value;
+      },
+      2,
+    ),
+  );
 
   return target;
 }
