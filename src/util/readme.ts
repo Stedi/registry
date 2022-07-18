64fdfcd3bd8c874edca726d94f90ac22c76fc423
Tomasz Łakomy <tomasz@stedi.com>
2022-07-18 11:59:22 +0200

    chore: update all paths in registry to /app/mappings (#132)

--- src/util/readme.ts
+++ src/util/readme.ts
@@ -19,12 +19,12 @@ export function generateMarkdownTableRow({ schemaName, providerName, target, doc
 
 function generateMapFromSchemaMarkdownButton({ target, providerName, schemaName }: GenerateMarkdownTableProps) {
   const mappingName = encodeURIComponent(`Mapping from ${capitalizeFirstLetter(providerName)}'s ${schemaName} schema`);
-  return `[![Map from this schema](/images/MapFromThisSchema.svg)](https://terminal.stedi.com/mappings/import?name=${mappingName}&referrer=registry-repo&source_json_schema=https://raw.githubusercontent.com/Stedi/registry/main/${target})`;
+  return `[![Map from this schema](/images/MapFromThisSchema.svg)](https://stedi.com/app/mappings/import?name=${mappingName}&referrer=registry-repo&source_json_schema=https://raw.githubusercontent.com/Stedi/registry/main/${target})`;
 }
 
 function generateMapToSchemaMarkdownButton({ target, providerName, schemaName }: GenerateMarkdownTableProps) {
   const mappingName = encodeURIComponent(`Mapping to ${capitalizeFirstLetter(providerName)}'s ${schemaName} schema`);
-  return `[![Map to this schema](/images/MapToThisSchema.svg)](https://terminal.stedi.com/mappings/import?name=${mappingName}&referrer=registry-repo&target_json_schema=https://raw.githubusercontent.com/Stedi/registry/main/${target})`;
+  return `[![Map to this schema](/images/MapToThisSchema.svg)](https://stedi.com/app/mappings/import?name=${mappingName}&referrer=registry-repo&target_json_schema=https://raw.githubusercontent.com/Stedi/registry/main/${target})`;
 }
 
 interface GenerateMarkdownTableProps {
